a3adfa0b71aa7dffd9a57b34d1ec51d6eb4def03
Aj Deveza <ajdvz0423@gmail.com>
2024-09-03 19:24:13 +0800

    added deviant image

--- src/pages/deviants/deviants.jsx
+++ src/pages/deviants/deviants.jsx
@@ -1,15 +1,21 @@
-import { CloudSVG, LeftRectangle, RightTriangle } from "@/assets";
+import { AngelDeviant, CloudSVG, LeftRectangle, RightTriangle } from "@/assets";
 import { StatsContainer } from "@/components/stats-container";
 
 const Deviants = () => {
   return (
-    <div className="flex flex-col bg-[#00CEB5] min-h-screen ">
+    <div className="bg-[#00CEB5]">
       <div
-        style={{ background: `center no-repeat url(${CloudSVG})`, backgroundSize: "contain" }}
-        className="min-h-screen"
+        style={{ background: `center no-repeat url(${CloudSVG})`, backgroundSize: "cover" }}
+        className="flex flex-col min-h-screen space-y-16"
       >
         <div>
           <StatsContainer />
+        </div>
+        <div>
+          <img
+            src={AngelDeviant}
+            alt="Deviant"
+          />
         </div>
         <div className="flex justify-between">
           <LeftRectangle />
