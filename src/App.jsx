ae3d433657ab96f3eaa20db840bbe893cbadbfe7
ddeveza <ddeveza@ymail.com>
2024-09-03 11:34:37 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+  return (
+    <>
+      <p1 className='text-red-700'>Hello World</p1>
+      <Button variant='outline'>Test</Button>
+    </>
+  )
+}
+
+export default App
