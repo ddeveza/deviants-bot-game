530f5ad1293742626be8a4bc1996dd2c215222d7
Aj Deveza <ajdvz0423@gmail.com>
2024-09-04 15:34:31 +0800

    added full upgrade items

--- src/pages/deviants/deviants.jsx
+++ src/pages/deviants/deviants.jsx
@@ -7,25 +7,22 @@ import { UpgradeModal } from "@/components/upgrade-modal";
 
 const Deviants = () => {
   return (
-    <div className="flex bg-[#00CEB5] flex-1 border border-blue-500 h-full">
-      <div
-        style={{ background: `center no-repeat url(${CloudSVG})`, backgroundSize: "cover" }}
-        className="flex flex-col border border-red-500 h-auto w-full justify-between"
-      >
-        <div className=" flex items-center justify-center border">
-          <StatsContainer />
-        </div>
-        <div className="flex items-center justify-center w-full border">
-          <DeviantsCarousel />
-        </div>
-        <div className="flex justify-between z-10 border pb-10">
-          <UpgradeModal>
-            <UpgradeButton />
-          </UpgradeModal>
-          <StatsButton />
-        </div>
+    <div
+      style={{ background: `center no-repeat url(${CloudSVG}) #00CEB5`, backgroundSize: "cover" }}
+      className="flex flex-col border border-red-500 w-full justify-between flex-1 h-full"
+    >
+      <div className=" flex items-center justify-center border">
+        <StatsContainer />
       </div>
-      ;
+      <div className="flex items-center justify-center w-full border">
+        <DeviantsCarousel />
+      </div>
+      <div className="flex justify-between z-10 border pb-10">
+        <UpgradeModal>
+          <UpgradeButton />
+        </UpgradeModal>
+        <StatsButton />
+      </div>
     </div>
   );
 };
