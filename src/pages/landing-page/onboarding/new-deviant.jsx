fb5d91cbe4bde438d5c3ac2fc34a69c109e8f7a4
ddeveza <ddeveza@ymail.com>
2024-09-04 10:03:02 +0800

    completed onboarding

--- src/pages/landing-page/onboarding/new-deviant.jsx
+++ src/pages/landing-page/onboarding/new-deviant.jsx
@@ -1,11 +1,30 @@
-import { flyAngelDeviant } from "@/assets";
+import { flyAngelDeviant, loreBG } from "@/assets";
 
 const NewDeviant = () => {
   return (
     <div
-      className="min-h-screen"
-      style={{ background: `top no-repeat url(${flyAngelDeviant})`, backgroundSize: "contain", backgroundPositionY: "60px" }}
-    ></div>
+      className="min-h-screen flex flex-col items-center justify-center space-y-10"
+      style={{ background: `no-repeat url(${loreBG})`, backgroundColor: "currentcolor" }}
+    >
+      <div className="text-white text-center font-chakra text-2xl font-bold leading-none uppercase max-w-52">
+        <div>Meet the</div>
+        <div>Angel Deviant</div>
+      </div>
+      <div>
+        <img
+          src={flyAngelDeviant}
+          alt="fly-angel"
+        />
+      </div>
+      <div>
+        <button
+          className="rounded-[20px] py-2.5 px-8 text-[161313] bg-white font-extrabold uppercase"
+          onClick={() => {}}
+        >
+          Continue
+        </button>
+      </div>
+    </div>
   );
 }
 
