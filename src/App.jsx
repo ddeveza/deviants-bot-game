2112690a8933a03dd3d1135d20df5e1ffb240f05
ddeveza <ddeveza@ymail.com>
2024-09-04 10:38:00 +0800

    fixed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,49 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Deviants from "./pages/deviants/deviants"
-import Incubation from "./pages/incubation/incubation"
-import LandingPage from "./pages/landing-page/landing-page"
-import Onboarding from "./pages/landing-page/onboarding/onboarding"
-import { PAGES } from "./constant/pages"
-import Lore from "./pages/landing-page/lore/lore"
-import { Navbar } from "./components/navbar"
-
+import { createBrowserRouter, RouterProvider, useLocation } from "react-router-dom";
+import Deviants from "./pages/deviants/deviants";
+import Incubation from "./pages/incubation/incubation";
+import LandingPage from "./pages/landing-page/landing-page";
+import Onboarding from "./pages/landing-page/onboarding/onboarding";
+import { PAGES } from "./constant/pages";
+import Lore from "./pages/landing-page/lore/lore";
+import { Navbar } from "./components/navbar";
 
 const router = createBrowserRouter([
   {
-    path:PAGES.home,
-    element:<LandingPage/>
+    path: PAGES.home,
+    element: <LandingPage />,
   },
   {
-    path:PAGES.deviants,
-    element:<Deviants/>
-
+    path: PAGES.deviants,
+    element: (
+      <>
+        <Deviants /> 
+        <Navbar />
+      </>
+    ),
   },
   {
-    path:PAGES.incubation,
-    element:<Incubation/>
+    path: PAGES.incubation,
+    element: (
+      <>
+        <Incubation />
+        <Navbar />
+      </>
+    ),
   },
   {
-    path:PAGES.onboard,
-    element:<Onboarding/>
+    path: PAGES.onboard,
+    element: <Onboarding />,
   },
   {
-    path:PAGES.lore,
-    element:<Lore/>
-  }
-
-])
-
+    path: PAGES.lore,
+    element: <Lore />,
+  },
+]);
 
 function App() {
   return (
     <>
       <RouterProvider router={router} />
-      <Navbar />
     </>
   );
 }
