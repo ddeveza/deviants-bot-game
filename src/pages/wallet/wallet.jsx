ee43a5f9e498aab288a7f74db520480baa1e59ad
ddeveza <ddeveza@ymail.com>
2024-09-05 19:48:50 +0800

    fixed position

--- src/pages/wallet/wallet.jsx
+++ src/pages/wallet/wallet.jsx
@@ -23,17 +23,21 @@ const Wallet = () => {
           Deviant
         </div>
 
-        <div className="pt-36 flex flex-col space-y-3 ">
-          <button
-            className="w-[23rem] font-bowlby text-white rounded-[20px] py-2.5 px-8 text-[161313] bg-[#929292]"
-            style={{
-              boxShadwdow: "box-shadow: 2px 3px 0px 1px rgba(0, 0, 0, 0.20);",
-              WebkitBoxShadow: "3px 6px 5px 0px rgba(22, 19, 19, 0.75) 100%)",
-            }}   
-            onClick={() => setConnected(!connected)}
-          >
-            Copy Address
-          </button>
+        <div className="pt-20 grid grid-rows-2 justify-center items-center space-y-3 ">
+          {connected ? (
+            <button
+              className="w-[23rem] font-bowlby text-white rounded-[20px] py-2.5 px-8 text-[161313] bg-[#929292]"
+              style={{
+                boxShadwdow: "box-shadow: 2px 3px 0px 1px rgba(0, 0, 0, 0.20);",
+                WebkitBoxShadow: "3px 6px 5px 0px rgba(22, 19, 19, 0.75) 100%)",
+              }}
+              onClick={() => setConnected(!connected)}
+            >
+              Copy Address
+            </button>
+          ) : (
+            <div />
+          )}
           <button
             className="w-[23rem] font-bowlby rounded-[20px] py-2.5 px-8 text-[161313] bg-white"
             style={{
