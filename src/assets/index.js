2d251302bb2665f68ba9e97e11aaa2a1b3f8aa4d
Aj Deveza <ajdvz0423@gmail.com>
2024-09-05 16:09:11 +0800

    added stat modal

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,6 @@
 import AngelDeviant from "./images/Angel.png";
 import deviantLogo from "./images/Deviant logo Lg_White.png";
-import { default as flyDevilDeviant, default as flyDevilImg } from "./images/Devil_2.png";
+import flyDevilDeviant from "./images/Devil_2.png";
 import flyAngelDeviant from "./images/flyAngelDeviant.png";
 import flyAngelImg from "./images/Flying-Angel.png";
 import loreBG from "./images/lore-bg.png";
@@ -16,12 +16,11 @@ import { BubbleSVG } from "./svg/bubble-svg";
 import { CarouselLeft, CarouselRight, ShopButton } from "./svg/button-svg";
 import CloudSVG from "./svg/cloud.svg";
 
+import incubatorImg from "./images/Incubator.png";
+import incubatorsUpgradeSVG from "./svg/Deviants Sm_Red.svg";
 import { DeviantsSVG, ShopSVG, TaskSVG, WalletSVG } from "./svg/nav-svg";
 import { ShardSVG } from "./svg/shard-svg";
-import statSVG from "./svg/stat-bg.svg";
 import { UpgradeSVG } from "./svg/upgrade-svg";
-import incubatorImg from './images/Incubator.png';
-import incubatorsUpgradeSVG from './svg/Deviants Sm_Red.svg'
 export {
   AngelDeviant,
   BubbleSVG,
@@ -34,8 +33,9 @@ export {
   flyAngelDeviant,
   flyAngelImg,
   flyDevilDeviant,
-  flyDevilImg,
   HpSVG,
+  incubatorImg,
+  incubatorsUpgradeSVG,
   lore1,
   lore2,
   lore3,
@@ -49,10 +49,7 @@ export {
   ShardSVG,
   ShopButton,
   ShopSVG,
-  statSVG,
   TaskSVG,
   UpgradeSVG,
   WalletSVG,
-  incubatorImg,
-  incubatorsUpgradeSVG
 };
