f8bfb37b62e2f51a6e9dadbc79fc5aa2abfc3db3
Aj Deveza <ajdvz0423@gmail.com>
2024-09-06 22:20:23 +0800

    adjusted light placing

--- src/pages/landing-page/components/onboarding2.jsx
+++ src/pages/landing-page/components/onboarding2.jsx
@@ -11,8 +11,8 @@ export const OnboardingTwo = () => {
         <LightSVG classname={"absolute top-[10%] left-[-40%] rotate-[320deg]"} />
         <LightSVG classname={"scale-x-[-1] absolute top-[10%] right-[-40%] rotate-[40deg]"} />
         {/* Bottom Middle */}
-        <LightSVG classname={"absolute bottom-[18%] right-[-8%] scale-y-[-1] rotate-[293deg]"} />
-        <LightSVG classname={"absolute bottom-[18%] left-[-8%] scale-x-[-1] scale-y-[-1] rotate-[60deg]"} />
+        <LightSVG classname={"absolute bottom-[15%] right-[-8%] scale-y-[-1] rotate-[293deg]"} />
+        <LightSVG classname={"absolute bottom-[15%] left-[-8%] scale-x-[-1] scale-y-[-1] rotate-[60deg]"} />
         {/* Bottom */}
         <LightSVG classname={"absolute bottom-0 left-0 scale-y-[-1]"} />
         <LightSVG classname={"absolute bottom-0 right-0 scale-x-[-1] scale-y-[-1]"} />
