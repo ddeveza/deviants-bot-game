43e78a7c9e5e510ee0c82b622b4e1c2c0c9e8bcf
ddeveza <ddeveza@ymail.com>
2024-09-06 15:23:21 +0800

    fixed bg color

--- src/pages/landing-page/landing-page.jsx
+++ src/pages/landing-page/landing-page.jsx
@@ -14,8 +14,8 @@ const LandingPage = () => {
   };
   return (
     <div
-      style={{ background: `top no-repeat url(${cloudSVG}) #CB1800`, backgroundSize: "cover", backgroundPositionY: "60px" }}
-      className="flex flex-col h-full"
+      style={{ background: `top no-repeat url(${cloudSVG}) `, backgroundSize: "cover", backgroundPositionY: "60px" }}
+      className="flex flex-col h-full #CB1800"
     >
       <div
         style={{ background: `top no-repeat url(${flyDevilDeviant})`, backgroundSize: "cover", backgroundPositionY: "60px" }}
