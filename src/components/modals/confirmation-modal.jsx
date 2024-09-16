a59feccafc6245e894cd453fc7fe31bef633928e
Aj <ajdvz0423@gmail.com>
2024-09-16 10:48:05 +0800

    added confirmation modal

---
+++ src/components/modals/confirmation-modal.jsx
@@ -0,0 +1,47 @@
+import { EnergySVG } from "@/assets";
+import { Modal } from "../modal";
+import { Button } from "../ui/button";
+import { DialogClose } from "../ui/dialog";
+
+export const ConfirmationModal = ({ children }) => {
+  return (
+    <Modal
+      title="Upgrade Deviant"
+      description="Upgrades a Deviant's stats"
+      triggerButton={children}
+      disabledCloseButton={true}
+      className="max-w-[360px] bg-[#BBB] border-none rounded-3xl sm:rounded-3xl p-0 min-h-[300px] translate-y-[-60%]"
+    >
+      <div className="flex flex-col bg-[#DCDCDC] rounded-3xl h-[98%] p-4 py-10 gap-6">
+        <div className="flex items-center m-auto w-max">
+          <div className="w-8 h-8">
+            <EnergySVG className="size-full" /> {/*Change Icon*/}
+          </div>
+          <p className="text-sm font-bowlby lining-nums proportional-nums tracking-[-0.56px]">upgrade max energy</p> {/*Change attribute*/}
+        </div>
+
+        <div className="flex justify-center">
+          <p className="font-chakra lining-nums proportional-nums tracking-[-0.64px] text-center leading-[20px] w-1/2">You are about to upgrade this Deviant’s Max Energy from LVL 1 to LVL 2</p>{" "}
+          {/*Change atttribute and lvl*/}
+        </div>
+
+        <div className="flex flex-col justify-center items-center mt-3 rounded-xl bg-[#FCFCFC] p-4 gap-1">
+          <p className="font-ibmMono lining-nums proportional-nums text-[8px] leading-[8px] uppercase">Costs</p>
+          <p className="font-bowlby text-xl leading-[20px] uppercase lining-nums proportional-nums">400XP</p> {/*Change cost*/}
+        </div>
+
+        <DialogClose asChild>
+          <div className="flex flex-col gap-2 lining-nums proportional-nums text-white tracking-[-0.64px] font-bowlby">
+            <Button className="bg-[#AAA] rounded-lg p-4 w-full">cancel</Button>
+            <Button
+              className="rounded-lg p-4 w-full"
+              style={{ background: "linear-gradient(90deg, #FF7C52 0%, #FF110B 100%)" }}
+            >
+              upgrade
+            </Button>
+          </div>
+        </DialogClose>
+      </div>
+    </Modal>
+  );
+};
--- src/components/stat-upgrades.jsx
+++ src/components/stat-upgrades.jsx
@@ -1,34 +1,37 @@
 import { cn } from "@/lib/utils";
+import { ConfirmationModal } from "./modals/confirmation-modal";
 
 export const StatUpgrade = ({ attribute, icon, currentLvl, xp, iconColor, isFullUpgrade }) => {
   const upgradeLevel = isFullUpgrade ? currentLvl + 4 : currentLvl + 1;
   return (
-    <div
-      className="flex gap-3 bg-[#E8E8E8] rounded-xl p-2 items-center"
-      style={{
-        background: isFullUpgrade ? "linear-gradient(90deg, #FFD700 0%, #A78D00 28%, #C6A700 47%, #BB9E00 76.5%, #FFD700 100%)" : "",
-      }}
-    >
+    <ConfirmationModal>
       <div
-        className={cn(`rounded-md w-10 h-10 p-1`)}
+        className="flex gap-3 bg-[#E8E8E8] rounded-xl p-2 items-center"
         style={{
-          background: isFullUpgrade ? "white" : iconColor,
+          background: isFullUpgrade ? "linear-gradient(90deg, #FFD700 0%, #A78D00 28%, #C6A700 47%, #BB9E00 76.5%, #FFD700 100%)" : "",
         }}
       >
-        {icon}
+        <div
+          className={cn(`rounded-md w-10 h-10 p-1`)}
+          style={{
+            background: isFullUpgrade ? "white" : iconColor,
+          }}
+        >
+          {icon}
+        </div>
+        <div className="flex flex-col space-y-1">
+          <p
+            className={cn("uppercase text-[8px] font-ibm leading-[8px] -tracking-[0.32px] font-bold proportional-nums lining-nums", isFullUpgrade ? "text-white" : "text-black")}
+          >{`upgrade ${attribute}`}</p>
+          <p className={cn("uppercase text-[8px] font-bold", isFullUpgrade ? "text-white" : "text-black")}>
+            {`LVL ${currentLvl} `}
+            <span className={cn(isFullUpgrade ? "text-[#FAFF00]" : "text-[#00AF9A]")}>&gt; {`LVL ${upgradeLevel}`}</span>
+          </p>
+          <p className={cn("uppercase text-[8px] proportional-nums font-bowlbyregular font-normal leading-[8px] -tracking-[0.48px]", isFullUpgrade ? "text-white" : "text-black")}>{`${xp} ${
+            isFullUpgrade ? "TON" : "XP"
+          }`}</p>
+        </div>
       </div>
-      <div className="flex flex-col space-y-1">
-        <p
-          className={cn("uppercase text-[8px] font-ibm leading-[8px] -tracking-[0.32px] font-bold proportional-nums lining-nums", isFullUpgrade ? "text-white" : "text-black")}
-        >{`upgrade ${attribute}`}</p>
-        <p className={cn("uppercase text-[8px] font-bold", isFullUpgrade ? "text-white" : "text-black")}>
-          {`LVL ${currentLvl} `}
-          <span className={cn(isFullUpgrade ? "text-[#FAFF00]" : "text-[#00AF9A]")}>&gt; {`LVL ${upgradeLevel}`}</span>
-        </p>
-        <p
-          className={cn("uppercase text-[8px] proportional-nums font-bowlbyregular font-normal leading-[8px] -tracking-[0.48px]", isFullUpgrade ? "text-white" : "text-black")}
-        >{`${xp} ${isFullUpgrade ? "TON" : "XP"}`}</p>
-      </div>
-    </div>
+    </ConfirmationModal>
   );
 };
